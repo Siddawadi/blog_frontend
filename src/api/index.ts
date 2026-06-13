a9875f397d9911471f43ea1e8164ab9888e8edbf
siddharthadawadi <siddharthadawadi10@gmail.com>
2026-06-13 12:30:24 +0545

    change base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"http://localhost:8080/blog",
+    baseURL:"https://blog-backeend.onrender.com/blog",
     withCredentials:true
 })
 export default instance
